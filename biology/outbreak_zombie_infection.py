efdb2fb2537a9d5a4579a1d4b13cb60714c8fdde
Breno.Andrade <andradefbreno@gmail.com>
2021-02-15 07:02:50 -0300

    Create outbreak_zombie_infection.py

---
+++ biology/outbreak_zombie_infection.py
@@ -0,0 +1,13 @@
+'''
+This code is based on the article "WHEN ZOMBIES ATTACK!: MATHEMATICAL MODELLING OF AN OUTBREAK OF ZOMBIE INFECTION" by Philip Munz; Ioan Hudea; Joe Imad; and Robert J. Smith;
+
+
+'''
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def zombie():
+    
